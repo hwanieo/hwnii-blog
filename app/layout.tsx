eeaf08a48e7fc0225b77fff8000c68627317d896
hwnii <hwniieo@gmail.com>
2024-07-27 02:10:09 +0900

    아니 빌드 성공이라는데 왜 너만 그래

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
 import '@/app/ui/globals.css';
+import Background from '@/app/ui/background';
+import Header from '@/app/ui/header';
 
 import { nunito } from '@/app/ui/fonts';
-import Background from './ui/background';
-import Header from './ui/header';
 
 export const metadata: Metadata = {
   title: 'hwnii blog',
