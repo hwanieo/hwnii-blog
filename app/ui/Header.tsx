079ce127e3b96c184ff833d34eb73229d05da4c0
hwnii <hwniieo@gmail.com>
2024-07-27 01:54:39 +0900

    build

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header className='bg-slate-300/50 border-slate-200/10 backdrop-filter backdrop-blur-sm z-20 flex items-center justify-between w-[90%] mt-4 px-10 py-4 rounded-md fixed left-1/2 md:w-[70%] transform -translate-x-1/2 tracking-tight'>
       {/* 로고 */}
       <Link href='/' className='flex gap-4 text-center items-center'>
-        <span className='flex items-center justify-center bg-black w-8 h-8 rounded-lg text-2xl text-white m-auto'>
+        <span className='flex items-center justify-center bg-black w-8 h-8 rounded-lg text-xl text-white m-auto'>
           H
         </span>
         <p className='uppercase text-xl'>Blog</p>
