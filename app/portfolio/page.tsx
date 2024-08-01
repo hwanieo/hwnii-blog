b3984364bde2b6dfb214eab69087ff9e81b074b9
hwnii <hwniieo@gmail.com>
2024-08-02 01:36:13 +0900

    쓸데없는 애니메이션 삭제함

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,7 +1,7 @@
 export default function PortfolioPage() {
   return (
     <main className='pt-28 w-[70%] h-screen mx-auto relative z-10 flex justify-center'>
-      <h1 className='text-3xl font-bold mt-72 animate-pulse'>Comming Soon!</h1>
+      <h1 className='text-3xl font-bold mt-72'>Comming Soon!</h1>
     </main>
   );
 }
